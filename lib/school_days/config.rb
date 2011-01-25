7ddb9ead83984f421c917e421037b6dc63d2523f
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-01-25 15:06:54 -0500

    better tests for config, reworked the interface for the config object, now throw a descriptive error if the config file is wrong

--- lib/school_days/config.rb
+++ lib/school_days/config.rb
@@ -1,16 +1,19 @@
 require 'singleton'
+require 'yaml'
+require 'date'
 
 module SchoolDays
 
   # controls the behavior of this gem.  Currently this gem only supports
   # loading information from a YAML file (TODO: implement programatic ways)
+  # ... well, you could do it, but I don't care about that code path yet...
   class ConfigBase
-    attr_accessor :sessions
+    attr_accessor :school_sessions
     attr_accessor :holiday_exceptions
     attr_accessor :included_day_exceptions
 
     def reset
-      self.sessions = {}
+      self.school_sessions = {}
       self.holiday_exceptions = []
       self.included_day_exceptions = []
     end
@@ -18,21 +21,32 @@ module SchoolDays
     def load(filename)
       reset
       data = YAML::load(File.open(filename))
-      self.sessions = data["school_days"]["school_sessions"]
+      sessions = data["school_days"]["school_sessions"]
 
+      self.school_sessions = sessions.collect do |session_name, session_value|
+        if session_value["start_date"].nil? || session_value["end_date"].nil?
+          raise "start_date or end_date is blank for #{session_name} in #{filename}"
+        end
+
+        {:start_date => Date.parse(session_value["start_date"]),
+          :end_date => Date.parse(session_value["end_date"])}
+      end
+      
       data["school_days"]["exceptions"]["holidays"].each do |holiday|
-        self.holiday_exceptions <<
-          ( Time.zone ? Time.zone.parse(holiday) : Time.parse(holiday) )
+        self.holiday_exceptions << Date.parse(holiday)
       end
 
       data["school_days"]["exceptions"]["included_days"].each do |extra_day|
-        self.included_day_exceptions <<
-          ( Time.zone ? Time.zone.parse(extra_day) : Time.parse(extra_day) )
+        self.included_day_exceptions << Date.parse(extra_day)
       end
     end
   end
 
   class Config < ConfigBase
     include Singleton
+  end
+
+  def self.config
+    Config.instance
   end
 end
--- test/test_config.rb
+++ test/test_config.rb
@@ -9,7 +9,7 @@ class TestConfig < Test::Unit::TestCase
   should "load a file with two sessions correctly" do
     conf = SchoolDays::ConfigBase.new
     assert_nothing_raised(Exception) {  conf.load( fixture_path() + "/double_session_test.yml" )  }
-    assert conf.sessions.length == 2
+    assert conf.school_sessions.length == 2
   end
 
   context "holiday exceptions" do
@@ -17,8 +17,8 @@ class TestConfig < Test::Unit::TestCase
       conf = SchoolDays::ConfigBase.new
       conf.load( fixture_path() + "/simple_test.yml" )
 
-      assert_contains( conf.holiday_exceptions, Time.parse("Jan 01, 2010") )
-      assert_does_not_contain( conf.holiday_exceptions, Time.parse("Jan 02, 2010") )
+      assert_contains( conf.holiday_exceptions, Date.parse("Jan 01, 2010") )
+      assert_does_not_contain( conf.holiday_exceptions, Date.parse("Jan 02, 2010") )
     end
   end
 
@@ -27,9 +27,15 @@ class TestConfig < Test::Unit::TestCase
       conf = SchoolDays::ConfigBase.new
       conf.load( fixture_path() + "/simple_test.yml" )
 
-      assert_contains( conf.included_day_exceptions, Time.parse("May 29, 2011") )
-      assert_does_not_contain( conf.included_day_exceptions, Time.parse("May 31, 2011") )
+      assert_contains( conf.included_day_exceptions, Date.parse("May 29, 2011") )
+      assert_does_not_contain( conf.included_day_exceptions, Date.parse("May 31, 2011") )
     end
   end
 
+  context "when loading an invalid config file" do
+    should "throw a Runtime error when we're missing start or end dates for a session" do
+      conf = SchoolDays::ConfigBase.new
+      assert_raise(RuntimeError) {  conf.load( fixture_path() + "/invalid_config_test.yml" )   }
+    end
+  end
 end
