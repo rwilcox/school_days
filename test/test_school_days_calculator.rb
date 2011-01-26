e4984d8be55c77bb7347f539a4df6bd836c15f65
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-01-25 22:43:53 -0500

    a commit just before the big rewrite of school days calculator

--- test/test_school_days_calculator.rb
+++ test/test_school_days_calculator.rb
@@ -13,10 +13,26 @@ class TestSchoolDaysCalculatorTest < Test::Unit::TestCase
         assert_equal( date + 1, 1.school_day.after(date) )
       end
 
-      should "move to Monday (if tomorrow is a weekend)"
-      should "move forward one week when adding 5 school days"
-      should "move forward onto an exceptional day"
-      should "skip exceptional holidays"
+      should "move to Monday (if tomorrow is a weekend)" do
+        date = Date.civil(2011, 10, 21) # a Friday
+        assert_equal( date + 3, 1.school_days.after(date) )
+      end
+
+      should "move forward one week when adding 5 school days" do
+        date = Date.civil(2011, 10, 17) # a Monday
+        assert_equal( date + 7, 5.school_days.after(date) )
+      end
+
+      should "move forward onto an exceptional day" do
+        date = Date.civil(2011, 5, 28)
+        assert_equal( date + 1, 1.school_day.after(date) )
+      end
+
+      should "skip exceptional holidays" do
+        date = Date.civil(2012, 4, 30) # is a Tuesday, but May 1 is a holiday
+        assert_equal( (date + 2).to_s, (1.school_day.after(date)).to_s )
+        # so we expect May 2
+      end
 
       should "move to the next session if we have multiple sessions"
       should "elegantly handle a situation where we go outside the school year"
